3efd1a3978e749821ad1dba8fa8bda5c262e8cd3
Mikkel O. Laursen <m@ikkel.dk>
2021-04-22 13:23:58 +0200

    Simplify data seeder

--- templates/restful_api/MyProject.Persistence/Seed/DataSeeder.cs
+++ templates/restful_api/MyProject.Persistence/Seed/DataSeeder.cs
@@ -7,17 +7,13 @@ namespace MyProject.Persistence.Seed
 {
     public static class DataSeeder
     {
-        public static async Task Initialize(IServiceProvider serviceProvider, bool createAdminUser = false)
+        public static async Task Initialize(IServiceProvider serviceProvider)
         {
             var provider = serviceProvider.CreateScope().ServiceProvider;
 
             using (var context = provider.GetRequiredService<AppDbContext>())
             {
                 context.Database.Migrate();
-
-                if (createAdminUser)
-                {
-                }
             }
         }
     }
